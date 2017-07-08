e64b2dc6cd81ae67d9506e533170898d670a1b76
Rob Bos <admin@ackspace.nl>
2017-07-08 17:26:14 +0200

    Formatting improvements
    Fixed indentation
    Removed some unneeded whitespace

--- stoplichter.ino
+++ stoplichter.ino
@@ -1,7 +1,5 @@
 void setup()
 {
-  // put your setup code here, to run once:
-
   pinMode( 5, OUTPUT );
   pinMode( 6, OUTPUT );
   pinMode( 7, OUTPUT );
@@ -13,7 +11,6 @@ void setup()
   pinMode( 11, OUTPUT );
   pinMode( 12, OUTPUT );
   pinMode( 13, OUTPUT );
-
 }
 
 #define BLINKDELAY 750
@@ -34,78 +31,74 @@ void loop()
   // All off
   off();
 
-#ifdef NORMALBLINK
-  for ( uint8_t n = 0; n < NORMALBLINK; ++n )
-  {
-    analogWrite( 8, 255 );
-    analogWrite( 9, ORANGEGREEN );
-    delay( BLINKDELAY );
-    analogWrite( 8, 0 );
-    analogWrite( 9, 0 );
-    delay( BLINKDELAY );
-  }
-#endif
-
-  // All off
-  off();
-
-
-#ifdef NORMALLIGHT
-  for ( uint8_t n = 0; n < NORMALLIGHT; ++n )
-  {
-    analogWrite( 5, 255 );
-    delay( 4000 );
-    analogWrite( 7, 0 );
-    analogWrite( 5, 0 );
-    analogWrite( 12, 255 );
-    delay( 8000 );
-    analogWrite( 12, 0 );
-    analogWrite( 8, 255 );
-    analogWrite( 9, ORANGEGREEN );
-
-    delay( 3000 );
-    analogWrite( 8, 0 );
-    analogWrite( 9, 0 );
-
-    analogWrite( 5, 255 );
-    delay( 3000 );
-    for ( uint8_t p = 0; p < 100; ++p )
+  #ifdef NORMALBLINK
+    for ( uint8_t n = 0; n < NORMALBLINK; ++n )
     {
-      // Pink!
-      analogWrite( 7, p );
-      delay( 20 );
+      analogWrite( 8, 255 );
+      analogWrite( 9, ORANGEGREEN );
+      delay( BLINKDELAY );
+      analogWrite( 8, 0 );
+      analogWrite( 9, 0 );
+      delay( BLINKDELAY );
     }
-  }
-  analogWrite( 7, 0 );
-#endif
+  #endif
 
   // All off
   off();
 
-#ifdef SPECIALBLINK
-  uint8_t r = 255;
-  uint8_t g = ORANGEGREEN;
-  uint8_t b = 0;
+  #ifdef NORMALLIGHT
+    for ( uint8_t n = 0; n < NORMALLIGHT; ++n )
+    {
+      analogWrite( 5, 255 );
+      delay( 4000 );
+      analogWrite( 7, 0 );
+      analogWrite( 5, 0 );
+      analogWrite( 12, 255 );
+      delay( 8000 );
+      analogWrite( 12, 0 );
+      analogWrite( 8, 255 );
+      analogWrite( 9, ORANGEGREEN );
+  
+      delay( 3000 );
+      analogWrite( 8, 0 );
+      analogWrite( 9, 0 );
+  
+      analogWrite( 5, 255 );
+      delay( 3000 );
+      for ( uint8_t p = 0; p < 100; ++p )
+      {
+        // Pink!
+        analogWrite( 7, p );
+        delay( 20 );
+      }
+    }
+    analogWrite( 7, 0 );
+  #endif
 
-  for ( uint8_t n = 0; n < SPECIALBLINK; ++n )
-  {
-    analogWrite( 8, r );
-    analogWrite( 9, g );
-    analogWrite( 10, b );
-    delay( BLINKDELAY );
-    analogWrite( 8, 0 );
-    analogWrite( 9, 0 );
-    analogWrite( 10, 0 );
-    delay( BLINKDELAY );
+  // All off
+  off();
 
-    r >>= 1;
-    g >>= 1;
-    b <<= 1;
-    if ( !b )
-      ++b;
-  }
-#endif
-
-
-
+  #ifdef SPECIALBLINK
+    uint8_t r = 255;
+    uint8_t g = ORANGEGREEN;
+    uint8_t b = 0;
+  
+    for ( uint8_t n = 0; n < SPECIALBLINK; ++n )
+    {
+      analogWrite( 8, r );
+      analogWrite( 9, g );
+      analogWrite( 10, b );
+      delay( BLINKDELAY );
+      analogWrite( 8, 0 );
+      analogWrite( 9, 0 );
+      analogWrite( 10, 0 );
+      delay( BLINKDELAY );
+  
+      r >>= 1;
+      g >>= 1;
+      b <<= 1;
+      if ( !b )
+        ++b;
+    }
+  #endif
 }
