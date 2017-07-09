88565fb12d3f06dc7c20dcff16e3c2f2b19aa74a
Rob Bos <admin@stuiterveer.com>
2017-07-09 12:51:21 +0200

    Add rainbow mode
    Every second there's a 1:300 chance that the light changes into a random different color

--- stoplichter.ino
+++ stoplichter.ino
@@ -1,6 +1,6 @@
 /*
- * Write this script to the Arduino
- */
+   Write this script to the Arduino
+*/
 
 String statusCode = "u";
 
@@ -32,6 +32,14 @@ void off()
   }
 }
 
+void rainbow(int lightPos) // 1=top; 2=middle; 3=bottom
+{
+  for (int n = lightPos * 3 + 2; n < lightPos * 3 + 5; ++n) // Starting position for every light is lightPos*3+2
+  {
+    analogWrite(n, random(0, 255));
+  }
+}
+
 void loop()
 {
   String statusInput = Serial1.readString();
@@ -43,18 +51,33 @@ void loop()
 
   off();
 
+  int lightSelector;
+  bool startRainbow = false;
+  if (random(300) == 42)
+  {
+    startRainbow = true;
+  }
+
   if (statusCode == "u")
   {
+    lightSelector = 2;
     analogWrite(8, 255);
     analogWrite(9, 110);
   }
   else if (statusCode == "o")
   {
+    lightSelector = 3;
     analogWrite(12, 255);
   }
   else if (statusCode == "c")
   {
+    lightSelector = 1;
     analogWrite(5, 255);
+  }
+
+  if (startRainbow)
+  {
+    rainbow(lightSelector);
   }
 
   delay(1000);
